9aaf0da83ce1aebc1f7e7df62550a18cd1c40ce8
TrpFrog <dev@trpfrog.net>
2024-03-30 11:53:05 +0900

    use hono

--- routes/photos.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.send('Some photos');
-});
-
-module.exports = router;
